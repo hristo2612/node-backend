98bcd6df671417bf4ab968577c0dd3197dfeafb3
hristo2612 <hristoapps@gmail.com>
2018-07-17 23:25:40 +0300

    Add setPassword and validPassword methods - used for validating and setting passwords with the help of hashes and salt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
@@ -11,5 +12,15 @@ var UserSchema = new mongoose.Schema({
 }, {timestamps: true});
 
 UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+
+UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+}
 
 mongoose.model('User', UserSchema);
