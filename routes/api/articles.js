5a8c0e583d6351c725e37b20b5e099b6bc5b46ad
hristo2612 <hristoapps@gmail.com>
2018-07-28 20:10:08 +0300

    Add Get Articles results with filtration

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -6,6 +6,64 @@ var User = mongoose.model('User');
 var Comment = mongoose.model('Comment');
 var auth = require('../auth');
 
+router.get('/', auth.optional, function(req, res, next) {
+    var query = {};
+    var limit = 20;
+    var offset = 0;
+
+    if (typeof req.query.limit  !== 'undefined') {
+        limit = req.query.limit;
+    }
+
+    if (typeof req.query.offset !== 'undefined') {
+        offset = req.query.offset;
+    }
+
+    if (typeof req.query.tag !== 'undefined' ) {
+        query.tagList = {"$in": [req.query.tag]};
+    }
+
+    Promise.all([
+        req.query.author ? User.findOne({username: req.query.author}) : null,
+        req.query.favorited ? User.findOne({username: req.query.favorited}) : null
+    ]).then(function(results){
+        var author = results[0];
+        var favoriter = results[1];
+
+        if (author) {
+            query.author = author._id;
+        }
+
+        if (favoriter) {
+            query._id = {$in: favoriter.favorites};
+        } else if (req.query.favorited) {
+            query._id = {$in: []};
+        }
+
+        return Promise.all([
+            Article.find(query)
+                .limit(Number(limit))
+                .skip(Number(offset))
+                .sort({createdAt: 'desc'})
+                .populate('author')
+                .exec(),
+            Article.count(query).exec(),
+            req.payload ? User.findById(req.payload.id) : null
+        ]).then(function(results){
+            var articles = results[0];
+            var articlesCount = results[1];
+            var user = results[2];
+    
+            return res.json({
+                articles: articles.map(function(article){
+                    return article.toArticleJSON(user);
+                }),
+                articlesCount: articlesCount
+            })
+        });
+    }).catch(next);
+});
+
 router.post('/', auth.required, function(req, res, next) {
     User.findById(req.payload.id).then(function(user) {
         if (!user) { return res.sendStatus(401); }
